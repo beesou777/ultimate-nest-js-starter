7aed1a7a0f1cb9b649f6b85e87ed6240179fcc00
beesou777 <sbeesou@gmail.com>
2023-12-12 19:29:06 +0545

    learned:test case pactum

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,8 +1,9 @@
 import { Test } from "@nestjs/testing";
 import { INestApplication, ValidationPipe } from "@nestjs/common";
-
+import * as pactum from "pactum"
 import { PrismaService } from "src/prisma/prisma.service";
 import { AppModule } from "src/app.module";
+import { AuthDto } from "src/auth/dto";
 
 describe('App e2e',()=>{
   let app : INestApplication;
@@ -30,7 +31,16 @@ describe('App e2e',()=>{
 
   describe('Auth',()=>{
     describe('Signup',()=>{
-      it.todo("Shound signup")
+      it("Shound signup",()=>{
+        const dto:AuthDto = {
+          email:'bishwa21@gmail.com',
+          password:'124'
+        }
+        return pactum.spec().post(
+          'http://localhost:3000/auth/signup'
+        ).withBody(dto)
+        .expectStatus(201)
+      })
     })
     describe('Signin',()=>{
       it.todo("Shound signin")
