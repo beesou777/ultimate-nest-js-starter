1b48a19d4a3bfa8693e1f80f07ab7bcecc609f54
beesou777 <sbeesou@gmail.com>
2023-12-13 22:40:22 +0545

    make a function to get all the boomark

--- src/bookmark/bookmark.service.ts
+++ src/bookmark/bookmark.service.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { CreateBookmarkDto } from './dto';
 import { EditBookmarkDto } from './dto/edit-bookmark.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class BookmarkService {
+    constructor(private prisma :PrismaService){}
     getBookmarks(
         userId: number
-    ) { }
+    ) { 
+        return this.prisma.bookmark.findMany({
+            where:{
+                userId
+            }
+        })
+    }
 
     getBookmarksById(
         userId: number,
